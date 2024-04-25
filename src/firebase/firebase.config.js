00e710ccfe2a5ebf95edf4e500495fb853f40a27
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-04-25 21:53:44 +0600

    firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArQBZQFb2n64JZM1vTBPK7w1H3tewmfdg",
+  authDomain: "tourism-app-f736e.firebaseapp.com",
+  projectId: "tourism-app-f736e",
+  storageBucket: "tourism-app-f736e.appspot.com",
+  messagingSenderId: "269904026184",
+  appId: "1:269904026184:web:bbf5f527d766f85910967d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

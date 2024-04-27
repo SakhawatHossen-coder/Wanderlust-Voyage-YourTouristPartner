a3a41fb20659195520eca0fd51eb13bcea318f78
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-04-27 11:30:36 +0600

    faq section edited and newsletter svg

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -3,44 +3,57 @@ import React from "react";
 const Faq = () => {
   return (
     <div>
-      <section className="bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800">
+      <section className="bg-[#5074a8] dark:bg-gray-100 text-gray-100 dark:text-gray-800 rounded-lg">
         <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
           <h2 className="text-2xl font-semibold sm:text-4xl">
-            Frequently Asked Questions
+            Europe 101: FAQs for Your European Adventure
           </h2>
-          <p className="mt-4 mb-8 text-gray-400 dark:text-gray-600">
-            Sagittis tempor donec id vestibulum viverra. Neque condimentum
-            primis orci at lacus amet bibendum.
+          <p className="mt-4 mb-8 text-[#0e1b1bef] dark:text-gray-600">
+            Unveiling the magic of Europe! Explore iconic landmarks,
+            breathtaking landscapes, and hidden gems. Discover the perfect
+            European adventure for you - cultural capitals, charming towns, and
+            stunning natural beauty await
           </p>
           <div className="space-y-4">
             <details className="w-full border rounded-lg">
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400 focus-visible:dark:ring-violet-600">
-                Ex orci laoreet egestas sapien magna egestas scelerisque?
+                Q: What are some of the most popular tourist destinations in
+                Europe?
               </summary>
-              <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400 dark:text-gray-600">
-                Lectus iaculis orci metus vitae ligula dictum per. Nisl per
-                nullam taciti at adipiscing est.{" "}
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-[#0e1b1bef] dark:text-gray-600">
+                A: Europe boasts a vast array of popular destinations, catering
+                to diverse interests. Here are a few highlights: Art and
+                Culture: Paris (France), Rome (Italy), Florence (Italy),
+                Amsterdam (Netherlands) History and Architecture: London
+                (England), Barcelona (Spain), Prague (Czech Republic), Berlin
+                (Germany) Natural Beauty: Swiss Alps (Switzerland), Amalfi Coast
+                (Italy), Scottish Highlands (Scotland), Norwegian fjords
+                (Norway) Beaches and Relaxation: Balearic Islands (Spain), Greek
+                Islands (Greece), French Riviera (France), Algarve (Portugal)
               </p>
             </details>
             <details className="w-full border rounded-lg">
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400 focus-visible:dark:ring-violet-600">
-                Lorem at arcu rutrum viverra metus sapien venenatis lobortis
-                odio?
+                Q: What is the best time to visit Europe?
               </summary>
-              <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400 dark:text-gray-600">
-                Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna
-                porttitor egestas tincidunt neque vehicula potenti.{" "}
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-[#0e1b1bef] dark:text-gray-600">
+                A: The ideal time depends on your preferences. Spring
+                (April-May) and fall (September-October) offer pleasant
+                temperatures and fewer crowds. Summer (June-August) is peak
+                season with warm weather, but also comes with higher prices and
+                larger crowds. Winter (December-February) offers festive charm
+                in many cities and excellent conditions for winter sports.
               </p>
             </details>
             <details className="w-full border rounded-lg">
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400 focus-visible:dark:ring-violet-600">
-                Eleifend feugiat sollicitudin laoreet adipiscing bibendum
-                suscipit erat?
+                Q: What currency is used in Europe?
               </summary>
-              <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-400 dark:text-gray-600">
-                Justo libero tellus integer tincidunt justo semper consequat
-                venenatis aliquet imperdiet. Ultricies urna proin fusce nulla
-                pretium sodales vel magna et massa euismod vulputate sed.{" "}
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-[#0e1b1bef] dark:text-gray-600">
+                A: The Euro (€) is the official currency in many European
+                countries. However, some countries like the United Kingdom
+                (Pound Sterling) and Switzerland (Swiss Franc) have their own
+                currencies.
               </p>
             </details>
           </div>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Typography } from "@material-tailwind/react";
 import NewsletterForm from "./NewsletterForm";
 // import { Input, Button } from "@material-tailwind/react";
+import newsletter from "../assets/newsletter.svg";
 const Newsletter = () => {
   const [email, setEmail] = React.useState("");
   const onChange = ({ target }) => setEmail(target.value);
@@ -9,10 +10,7 @@ const Newsletter = () => {
     <div className="my-10">
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
+          <img src={newsletter} className="max-w-sm rounded-lg shadow-2xl" />
           <div>
             <h1 className="text-5xl font-bold">Sign Up for Our Newsletter</h1>
             <p className="py-6">
